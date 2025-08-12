77130db2db73afdd5b0e75b2e899d992b62fe337
Tetsu <htetsu0227@gmail.com>
2025-08-12 12:39:50 +0900

    feat: Add About component with short self-introduction

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm Tetsuto HARA, a passionate front-end developer.
+        I enjoy building clean, efficient, and user-friedly web applications.
+      </p>
+    </section>
+  )
+} 
+
+export default About;
