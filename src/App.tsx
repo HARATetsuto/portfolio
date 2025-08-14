e5b9bdfddd002d631f3497be1b1653c5973f19f7
Tetsu <htetsu0227@gmail.com>
2025-08-14 12:09:32 +0900

    feat: Add Contanct form with EmailJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Career from "./components/Career";
 import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
+import Contact from "./components/Contact";
 // import Footer from "./components/Footer";
 import "./styles/global.css";
 
@@ -17,7 +16,7 @@ function App() {
 				<Skills />
 				<Career />
 				<Projects />
-				{/* <Contact /> */}
+				<Contact />
 			</main>
 			{/* <Footer /> */}
 		</>
---
+++ src/components/Contact.tsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+
+const Contact = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+
+      <label>Your Email</label>
+      <input type="email" name="user_email" />
+
+      <label>Message</label>
+      <input type="message" />
+
+      <input type="submit" value="Send" />
+    </form>
+  )
+}
+
+export default Contact;
