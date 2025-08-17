9063dfbb5f07624ec69d6b7ca74d4405d8d21c2d
Tetsu <htetsu0227@gmail.com>
2025-08-17 21:18:09 +0900

    feat: Style career page with flex box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,6 +16,7 @@ const Header: React.FC = () => {
 				<ul>
 					<li><a href="#about">About</a></li>
 					<li><a href="#skills">Skills</a></li>
+					<li><a href="#career">Career</a></li>
 					<li><a href="#projects">Projects</a></li>
 					<li><a href="#contact">Contact</a></li>
 				</ul>
