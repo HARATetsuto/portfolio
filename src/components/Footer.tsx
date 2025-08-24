6cd74194b8c9d5e0ae943b98ed967d37d17fad49
Tetsu <htetsu0227@gmail.com>
2025-08-24 10:58:46 +0900

    feat: Add styles to footer part

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import LinkedInIcon from "../assets/linkedin.svg?react";
 
 const Footer: React.FC = () => {
+	const ICON_SIZE = 30;
   return (
     <footer>
-      <nav>
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+
+      <div className="left-components">
+				<a href="https://www.linkedin.com/in/tetsuto-hara-7822b5257/" target="_blank" id="header-title">
+					<LinkedInIcon width={ICON_SIZE} height={ICON_SIZE}/>
+				</a>
+      </div>
+
+      <div className="right-conponents">
+        <nav>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
     </footer>
   )
 }
